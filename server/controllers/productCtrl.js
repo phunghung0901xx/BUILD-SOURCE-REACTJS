0eb064ffc645554d3319d59a25f40ed2e05a8936
phunghung0901xx <phunghung0901xx@gmail.com>
2023-03-29 23:02:23 +0700

    update source

--- server/controllers/productCtrl.js
+++ server/controllers/productCtrl.js
@@ -17,15 +17,15 @@ const createProduct = asyncHandler(async (req, res) => {
 })
 
 const updateProduct = asyncHandler(async (req, res) => {
-    const id = req.params.id   
+    const id = req.params.id
     try {
         if (req.body.title) {
             req.body.slug = slugify(req.body.title)
         }
         const updateProduct = await Product.findOneAndUpdate(
-             id, 
-             req.body, 
-             {new:true}
+            id,
+            req.body,
+            { new: true }
         )
         res.json(updateProduct)
     }
@@ -64,20 +64,40 @@ const getaProduct = asyncHandler(async (req, res) => {
 const getAllProduct = asyncHandler(async (req, res) => {
     try {
         //Filtering  
-        const queryObj = {...req.query}
-        const excludeFields = ['page','sort','limit','fields']
-        excludeFields.forEach(el => delete queryObj[el] )
+        const queryObj = { ...req.query }
+        const excludeFields = ['page', 'sort', 'limit', 'fields']
+        excludeFields.forEach(el => delete queryObj[el])
         let queryStr = JSON.stringify(queryObj)
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
-        console.log(JSON.parse(queryStr))
+       
 
-        const query = Product.find(JSON.parse(queryStr))
+        let query = Product.find(JSON.parse(queryStr))
 
         // Sorting
-        if(req.query.sort) {
-            query
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join("")
+            query = query.sort(sortBy)
         } else {
+            query = query.sort('-createdAt')
+        }
 
+        // Limiting the fields
+        if (req.query.fields) {
+            const fields = req.query.flieds.sort.split(',').join("")
+            query = query.select(fields)
+        }
+        else {
+            query = query.select("-__v")
+        }
+
+        // pagination
+        const page = req.query.page
+        const limit = req.query.limit
+        const skip = (page -1) * limit
+        query = query.skip(skip).limit(limit)
+        if(req.query.page) {
+            const productCount = await Product.countDocuments()
+            if (skip >= productCount) throw new Error('This Page does not exists')
         }
         const product = await query
         res.json(product)
@@ -87,8 +107,8 @@ const getAllProduct = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = 
-{ 
+module.exports =
+{
     createProduct,
     getaProduct,
     getAllProduct,
